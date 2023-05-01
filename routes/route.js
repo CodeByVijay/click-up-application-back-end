3d5c68c7c259666aba01441d54127c3c29545ce0
Vijay Amule <gitbyvijay@gmail.com>
2023-05-01 19:02:11 +0530

    project insert

--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,20 @@
-import { Register,Login } from "../controller/UserController.js";
+import {
+  Register,
+  Login,
+  UserList,
+  StoreProject,
+  getAllProjects,
+  getSingleProject
+} from "../controller/UserController.js";
 import express, { json } from "express";
 const router = express.Router();
 
 router.post("/api/register", Register);
 router.post("/api/login", Login);
+router.get("/api/user-list", UserList);
+router.post("/api/store-project", StoreProject);
+router.get("/api/all-projects", getAllProjects);
+router.post("/api/project", getSingleProject);
 
 router.all("*", (req, res) => {
   res.status(200).json({ result: "failed", msg: "url not found." });
